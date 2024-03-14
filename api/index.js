4680d114f31f12b6b950f1a9fe0026b5219b0315
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-03-13 19:43:39 -0600

    minor correction

--- api/index.js
+++ api/index.js
@@ -12,31 +12,32 @@ export default function startNamespace(req, res) {
 	if (!namespace) {
 		console.log("starting basicChatroom namespace");
 
-		namespace = io.of("/basicChatroom")
-			.on("connection", async (socket) => {
-				// wake up pocketbase
-				startPocketbase(req, res);
+		namespace = io.of("/basicChatroom");
 
-				// get chat history
-				const messages = await pocketbase.collection("basicChatroom").getFullList({
-					sort: "-date",
-				});
+		namespace.on("connection", async (socket) => {
+			// wake up pocketbase
+			startPocketbase(req, res);
 
-				// send chat history to user
-				socket.emit("messages", messages.reverse());
-
-				// listen for messages
-				socket.on("message", async (content) => {
-					// create message object
-					const message = new Message("Anonymous", content, new Date());
-
-					// broadcast message to all of namespace
-					namespace.emit("messages", [message]);
-
-					// add message to database
-					await pocketbase.collection("basicChatroom").create(message);
-				});
+			// get chat history
+			const messages = await pocketbase.collection("basicChatroom").getFullList({
+				sort: "-date",
 			});
+
+			// send chat history to user
+			socket.emit("messages", messages.reverse());
+
+			// listen for messages
+			socket.on("message", async (content) => {
+				// create message object
+				const message = new Message("Anonymous", content, new Date());
+
+				// broadcast message to all of namespace
+				namespace.emit("messages", [message]);
+
+				// add message to database
+				await pocketbase.collection("basicChatroom").create(message);
+			});
+		});
 	}
 
 	res.end();
