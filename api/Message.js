976c1879e5c512895182e28ce1da4efc26eaa913
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-09-19 22:37:43 -0600

    use export default

--- api/Message.js
+++ api/Message.js
@@ -1,9 +1,7 @@
-class Message {
+export default class Message {
 	constructor(sender, content, date) {
 		this.sender = sender;
 		this.content = content;
 		this.date = date;
 	}
 }
-
-export default Message;
